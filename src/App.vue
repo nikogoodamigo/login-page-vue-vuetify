d52d0eb1e225db7c0438e25c3f08b8d10693ece0
Danny <dn.webdev@yahoo.com>
2023-04-27 19:44:21 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app>
+    <v-main>
+      <LoginPage />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LoginPage from './components/LoginPage.vue';
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
+    LoginPage
+},
+
+  data: () => ({
+    //
+  }),
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/LoginPage.vue
@@ -0,0 +1,71 @@
+<template>
+      <v-sheet class="login-form mx-auto rounded-lg">
+        <v-row>
+            <h1 class="text-center">Log In</h1>
+        </v-row>
+        <v-form>
+          <v-row class="user-input">
+            <v-text-field v-model="userName" :rules="rule1" label="Username"></v-text-field>
+          </v-row>
+          <v-row class="user-input">
+            <v-text-field v-model="password" :rules="rule2" label="Password" required></v-text-field>
+          </v-row>
+          <v-row class="user-input">
+            <v-text-field v-model="token" :rules="rule3" label="Token" required></v-text-field>
+          </v-row>
+          <v-row class="user-input">
+            <v-btn type="submit" size="large" variant="elevated"> SUBMIT </v-btn>
+          </v-row>
+        </v-form>
+      </v-sheet>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      userName: '',
+      rule1: [
+        value => {
+          if (value) return true
+
+          return 'You must enter a username.'
+        },
+      ],
+      password: '',
+      rule2: [
+        value => {
+            if (value) return true
+            return 'You must enter a password.'
+        },
+      ],
+      token: '',
+      rule3: [
+        value => {
+            if (value) return true
+            return 'You must enter a token.'
+        },
+      ]
+      
+    }),
+  }
+</script>
+
+<style scoped>
+h1 {
+    margin-bottom: 25px;
+    font-size: 3rem;
+}
+
+.login-form{
+    width: 30%;
+    margin-top: 10%;
+    padding: 25px 50px;
+    border-width: 2px;
+    border-color: grey;
+}
+
+.user-input{
+    margin: 10px 0;
+}
+
+</style>
