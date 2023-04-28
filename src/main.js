2e6c640f715c465d09691247a47ca7a3aebd579b
Danny <dn.webdev@yahoo.com>
2023-04-28 13:30:57 +0200

    third commit

--- src/main.js
+++ src/main.js
@@ -2,9 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
+import Vuex from 'vuex'
+import axios from 'axios'
+
 
 loadFonts()
 
 createApp(App)
   .use(vuetify)
+  .use(Vuex)
+
+  axios.defaults.baseURL = ''
   .mount('#app')
