089e4f1fcc65f79fdaf529887d2fef7d8b9854e3
Danny <dn.webdev@yahoo.com>
2023-05-05 23:26:46 +0200

    c11: added api-connection.js and DataRetreive.vue

--- src/router.js
+++ src/router.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import LoginPage from '@/components/LoginPage.vue';
 import AdminPanel from '@/components/AdminPanel.vue';
+import DataRetreive from '@/components/DataRetreive.vue';
 
 export const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', component: LoginPage },
-    { path: '/adminpanel', component: AdminPanel}
+    { path: '/adminpanel', component: AdminPanel},
+    { path: '/test', component: DataRetreive}
   ]
 });
